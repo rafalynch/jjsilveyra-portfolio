dfe9a897da1bbb7b3f9951035ea20f81fa4fce14
Rafael Lynch <rafalynch.dev@gmail.com>
2022-03-10 15:11:32 -0300

    Created pages

---
+++ pages/bio/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/Layout";
+
+export default function Collections() {
+  return (
+    <>
+      <Layout>
+        <div>Bio</div>
+      </Layout>
+    </>
+  );
+}
---
+++ pages/gallery/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Layout from "../../components/Layout";
+
+function Gallery() {
+  return (
+    <>
+      <Layout>
+        <div>Gallery</div>
+      </Layout>
+    </>
+  );
+}
+
+export default Gallery;
