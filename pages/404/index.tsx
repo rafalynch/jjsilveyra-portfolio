0560a624b632f8c69fd3928a9d106389eed4fca0
Rafael Lynch <rafalynch.dev@gmail.com>
2022-03-10 15:10:08 -0300

    Add 404 page

---
+++ pages/404/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./index.module.css";
+
+import Layout from "../../components/Layout";
+
+export default function () {
+  return (
+    <div>
+      <Layout>
+        <div className={styles.container}>404 - No existe esta página</div>
+      </Layout>
+    </div>
+  );
+}
