2669ca5ff65ad38471e6a57bbce7c98de6bbfe4d
rafalynch <rafalynch.dev@gmail.com>
2022-07-16 15:42:20 -0300

    Fix home image mobile

--- components/HomeImage/index.tsx
+++ components/HomeImage/index.tsx
@@ -3,12 +3,15 @@ import React from "react";
 import Image from "next/image";
 import { getHomeImage } from "../../services/api";
 import styles from "./index.module.css";
+import useMediaQuery from "@mui/material/useMediaQuery";
 
 interface HomeImageProps {
   homeImage: Asset;
 }
 
 function HomeImage({ homeImage }: HomeImageProps) {
+  const mobile = useMediaQuery("(max-width:600px)");
+
   return (
     <Image
       className={styles.image}
@@ -16,6 +19,7 @@ function HomeImage({ homeImage }: HomeImageProps) {
       objectFit="cover"
       src={"https:" + homeImage.fields.file.url}
       alt="home"
+      objectPosition={mobile ? "70%" : "center"}
     ></Image>
   );
 }
